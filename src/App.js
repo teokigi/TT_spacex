8c3cae43febbc614ffb89dfcaf620d3a59bdf8a3
Teo <43215755+teokigi@users.noreply.github.com>
2020-12-03 11:41:27 +0000

    code general layout

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        Space X App
       <header className="App-header">
         
         <p>
-            space X
+            space X Header
         </p>
         
       </header>
+      <body className="App-body">
+        <p>
+            Space X Body
+        </p>
+      </body>
     </div>
   );
 }
