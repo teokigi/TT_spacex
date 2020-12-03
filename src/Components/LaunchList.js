e5d2ab58c5c81740ac81c1a769155c3ecc70401a
Teo <43215755+teokigi@users.noreply.github.com>
2020-12-03 19:56:08 +0000

    filter by year works.

--- src/Components/LaunchList.js
+++ src/Components/LaunchList.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LaunchList = (props) =>{
+    const [selYear,setselYear] = useState({year:null})
     if (props ==null){
         return ("loading/no launches...")
     }
@@ -13,6 +14,14 @@ const LaunchList = (props) =>{
         )
     })
 
+    const filteredLaunches = props.launchData.filter(launch=>launch.date_utc.slice(0,4)===selYear.year)
+        .map((launch, index)=>{
+        return(
+            <div id="launchlist" key={index}>
+                {launch.name},{launch.date_utc.slice(0,10)}
+            </div>
+        )
+    })
     const yearOptions = props.launchData.map(launch=>launch.date_utc.slice(0,4))
         .filter((year, index, array)=>array.indexOf(year)===index)
         .map((year, index)=>{
@@ -20,17 +29,25 @@ const LaunchList = (props) =>{
                 <option value={year} key={index}>{year}</option>
             )
         })
-
+    
+    if(selYear.year==null){
     return(
         <div>
-                     <select>
-                         <option> Filter By year</option>
+                     <select onChange={id=>setselYear({year:id.target.value})} defaultValue="default">
+                         <option disabled value="default"> Filter By year</option>
                         {yearOptions}
                      </select>
                      <button>Sort by Date</button>
         {allLaunches}
         </div>
     )
+    } else{
+        return(
+            <div>
+                {filteredLaunches}
+            </div>
+        )
+    }
 }
 
 export default LaunchList;
